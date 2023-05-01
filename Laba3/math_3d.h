ac413cc58f5f32022231ccc8b2c013a3c3465805
Ильфат <kilfat35@gmail.com>
2023-05-01 19:54:15 +0500

    Добавлен урок 15

--- Laba3/math_3d.h
+++ Laba3/math_3d.h
@@ -9,6 +9,12 @@ static const float M_PI = 3.1415;
 #define ToRadian(x) ((x) * M_PI / 180.0f)
 #define ToDegree(x) ((x) * 180.0f / M_PI)
 
+struct Vector2i
+{
+    int x;
+    int y;
+};
+
 struct Vector3f
 {
     float x;
@@ -57,11 +63,23 @@ struct Vector3f
 
     Vector3f& Normalize();
 
+    void Rotate(float Angle, const Vector3f& Axis);
+
     void Print() const
     {
         printf("(%.02f, %.02f, %.02f", x, y, z);
     }
 };
+
+
+inline Vector3f operator+(const Vector3f& l, const Vector3f& r)
+{
+    Vector3f Ret(l.x + r.x,
+        l.y + r.y,
+        l.z + r.z);
+
+    return Ret;
+}
 
 inline Vector3f operator-(const Vector3f& l, const Vector3f& r)
 {
@@ -123,5 +141,20 @@ public:
     void InitPersProjTransform(float FOV, float Width, float Height, float zNear, float zFar);
 };
 
+
+struct Quaternion
+{
+    float x, y, z, w;
+
+    Quaternion(float _x, float _y, float _z, float _w);
+
+    void Normalize();
+
+    Quaternion Conjugate();
+};
+
+Quaternion operator*(const Quaternion& l, const Quaternion& r);
+
+Quaternion operator*(const Quaternion& q, const Vector3f& v);
 
 #endif	/* MATH_3D_H */
